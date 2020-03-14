7f5b238ce7d04a918a3487d9ffa4d0b3f93ac6b5
Hanumeshjitta <39722076+Hanumeshjitta@users.noreply.github.com>
2020-03-14 23:53:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:Green;">Welcome to Wipro CloudStudio Team - Version 3 - Running on ECS!!!!!!</h1> \n');
+  res.send('<h1 style="color:Red;">Welcome to Wipro CloudStudio Team - Version 3 - Running on ECS!!!!!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
